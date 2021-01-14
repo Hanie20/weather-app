50a9b583cd178ec2fccfc315583180a25965b35f
Hanie20 <damas.johanie@courrier.uqam.ca>
2021-01-14 16:43:57 -0500

    api integration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.css';
 import Container from "./Container";
 
 function App() {
   return (
-    <div className="App">
+<div className="App">
       <div className="container">
-        <Container />
-      </div>
+        <Container defaultCity="New York"/>
+        <footer> 
+           <small className="projectLink">
+          <a href="https://github.com/Hanie20/weather-app.git" target="_blank" >Open-source code </a>
+        by <a  href="https://www.instagram.com/johaniedamas/" target="_blank">  Johanie Damas  </a>  
+        </small>
+        </footer>
+      </div>  
     </div>
   );
 }
